fc323f358d1a33e0e70923732ecc206b9ab1c27e
zayong <mase0804@naver.com>
2022-02-08 13:16:53 +0900

    update

--- src/index.js
+++ src/index.js
@@ -4,24 +4,33 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {createStore, applyMiddleware} from "redux";
-import {composeWithDevTools} from 'redux-devtools-extension';
 import {Provider} from "react-redux";
 import rootReducer from "./modules";
 import logger from 'redux-logger';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import ReduxThunk from 'redux-thunk';
-import {BrowserRouter} from 'react-router-dom';
+import {Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+const customHistory = createBrowserHistory();
 
 const store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(ReduxThunk, logger))
-);
+    // logger 를 사용하는 경우, logger가 가장 마지막에 와야합니다.
+    composeWithDevTools(
+        applyMiddleware(
+            ReduxThunk.withExtraArgument({ history: customHistory }),
+            logger
+        )
+    )
+); // 여러개의 미들웨어를 적용 할 수 있습니다.
 
 ReactDOM.render(
-    <BrowserRouter>
+    <Router history={customHistory}>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>,
+    </Router>,
     document.getElementById('root')
 );
 
